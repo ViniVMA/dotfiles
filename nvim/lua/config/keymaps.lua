465da07b973005fa253f9008c09ce3eb39c17e8b
ViniVMA <ctt.viniciusvilela@gmail.com>
2024-12-05 23:31:23 -0300

    update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,9 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
---
---
+
 vim.keymap.set(
   "n",
   "<leader>fl",
@@ -14,3 +12,18 @@ vim.keymap.set(
 vim.keymap.set("n", "<leader>fg", ":GrugFar<CR>", { desc = "GrugFar" })
 vim.keymap.set("n", "<C-j>", "j$", { desc = "Move down and to end of line" })
 vim.keymap.set("n", "<C-k>", "k$", { desc = "Move up and to end of line" })
+
+-- Move selected lines with shift+j or shift+k
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+
+-- clear search highlight
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Search and replace word under the cursor
+vim.keymap.set(
+  "n",
+  "<F2>",
+  [[:%s/\<<C-r><C-w>\>//g<Left><Left>]],
+  { desc = "Search and replace word under the cursor" }
+)
